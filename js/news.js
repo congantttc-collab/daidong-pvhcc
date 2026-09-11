796ec7013e792e033694e52e2209f7f089b44035
congantttc-collab <congantttc@gmail.com>
2026-09-11 08:20:42 +0700

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,37 +1,36 @@
-fetch("data/news.json")
-.then(res => res.json())
-.then(data => {
-
+fetch("../data/news.json")
+  .then(res => res.json())
+  .then(data => {
     const featured = document.getElementById("featured-news");
     const sidebar = document.getElementById("news-sidebar");
 
-    if(!featured || !sidebar) return;
+    if (!featured || !sidebar) return;
 
     const first = data[0];
 
-  featured.innerHTML = `
-    <a class="featured-card" href="pages/article.html?id=${first.id}">
-            <img src="${first.image}" alt="${first.title}">
-            <div class="featured-overlay">
-               <span class="featured-category">TIN NỔI BẬT</span>
-                <h3>${first.title}</h3>
-                <div class="featured-date">${first.date}</div>
-            </div>
-        </a>
+    featured.innerHTML = `
+      <a href="pages/article.html?id=${first.id}" class="featured-card">
+        <img src="${first.image}" alt="${first.title}">
+        <div class="featured-overlay">
+          <span class="featured-category">TIN NỔI BẬT</span>
+          <h3>${first.title}</h3>
+          <div class="featured-date">${first.date}</div>
+        </div>
+      </a>
     `;
 
     sidebar.innerHTML = "";
 
-    data.slice(1,5).forEach(item=>{
-        sidebar.innerHTML += `
-      <a class="news-mini" href="pages/article.html?id=${item.id}">
-            <img src="${item.image}" alt="${item.title}">
-            <div>
-                <small>${item.date}</small>
-               <h4>${item.title} →</h4>
-            </div>
+    data.slice(1, 5).forEach(item => {
+      sidebar.innerHTML += `
+        <a href="pages/article.html?id=${item.id}" class="news-mini">
+          <img src="${item.image}" alt="${item.title}">
+          <div>
+            <small>${item.date}</small>
+            <h4>${item.title} →</h4>
+          </div>
         </a>
-        `;
+      `;
     });
-
-});
+  })
+  .catch(err => console.error(err));
